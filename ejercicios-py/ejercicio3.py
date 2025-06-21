9a7b592af61be8816782015c09380205529bd4fd
emeling lilieth <est.elquinterob0201@uml.ni>
2025-06-20 22:07:25 -0600

    ejercicio 3 de python completado

--- ejercicios-py/ejercicio3.py
+++ ejercicios-py/ejercicio3.py
@@ -1,24 +1,2 @@
- # Lista de palabras
-palabras = [
-    'Aceituna', 'Murciélago', 'Educación', 'Aeropuerto',
-    'Otorrinolaringólogo', 'Euforia', 'Aceite', 'Paleontólogo',
-    'Hippópata', 'Arquitectura'
-]
-
-# Función para contar las vocales
-def contar_vocales(palabra):
-    vocales = 'AEIOUaeiou'
-    contador = 0
-    for char in palabra:
-        if char in vocales:
-            contador += 1
-    return contador
-
-# Seleccionar una palabra aleatoria
-import random
-indice_aleatorio = random.randint(0, len(palabras) - 1)
-palabra_aleatoria = palabras[indice_aleatorio]
-
-# Contar las vocales en la palabra aleatoria y mostrar el resultado
-vocales_en_palabra = contar_vocales(palabra_aleatoria)
-print(f"La palabra '{palabra_aleatoria}' tiene {vocales_en_palabra} vocales.")
+ 
+ 
